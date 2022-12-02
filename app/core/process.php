6b2a712886afbae2ae746f1e9ae9b3abdd395499
Salaou-Deen Paraïso <pariso03henri@gmail.com>
2022-12-02 18:00:59 +0100

    - update process dispatcher parameter

--- app/core/process.php
+++ app/core/process.php
@@ -2,8 +2,8 @@
 	// this file call and run the suitable logical instruction for our requests 
     require('views.php');
      
-    if(isset($_GET['process'])){
-        $_GET['process']();
+    if(isset($_GET['p'])){
+        $_GET['p']();
     }
     else{
         header('location: /index.html');
